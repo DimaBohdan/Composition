854c721a23696f6aa0001cd02ceed9d7e5b51c57
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-10-31 19:43:17 +0200

    tasks completed

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const pipe = (...fns) => (x) => null;
+const pipe = (...fns) => {
+  if (!fns.length) {
+    throw new Error('All compose arguments should be functions');
+  }
+  for (const fn of fns) {
+    if (typeof fn !== 'function') {
+      throw new Error('All compose arguments should be functions');
+    }
+  }
+  return (arg) => fns.reduce((acc, fn) => fn(acc), arg);
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,27 @@
 'use strict';
 
-const compose = (...fns) => (x) => null;
+const compose = (...fns) => {
+  const handlers = [];
+
+  const composed = (x) => {
+    let result = x;
+
+    try {
+      for (let i = fns.length - 1; i >= 0; i--) {
+        result = fns[i](result);
+      }
+      return result;
+    } catch (error) {
+      handlers.forEach((handler) => handler(error));
+      return null;
+    }
+  };
+
+  composed.on = (name, handler) => {
+    if (name === 'error') handlers.push(handler);
+  };
+
+  return composed;
+};
 
 module.exports = { compose };
